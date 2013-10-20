04e691e7a13db3911c678ddf7816597656bc8e1a
Karol Kulesza <karol.kulesza.2@gmail.com>
2013-10-20 21:28:56 +0200

    Eclipse project as Maven project established.

--- src/main/java/pl/solsoft/shop2013/validation/ConfirmUserPassword.java
+++ src/main/java/pl/solsoft/shop2013/validation/ConfirmUserPassword.java
@@ -5,15 +5,13 @@ import java.lang.annotation.*;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-*
+/**
  * Created with IntelliJ IDEA.
  * User: arek
  * Date: 14.08.13
  * Time: 16:21
  * To change this template use File | Settings | File Templates.
-
-
-
+*/
 
 @Target( {ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Documented
